04cc7d32e1d4bab5db7f8bb5306cb6806b6fac32
Rajath R Prasad <rajath2010rrp@gmail.com>
2026-04-22 23:14:54 +0530

    selectionsort corrected

--- sorting/selectionsort/selectionsort.c
+++ sorting/selectionsort/selectionsort.c
@@ -2,24 +2,21 @@
 #include<stdlib.h>
 #include<time.h>
 int count;
-void selectionsort(int *a,int n)
-{
-int i,j,min,t;
-for(i=0;i<n-1;i++)
-{
-min=i;
-for(j=i+1;j<n;j++)
-{if((a+j)<(a+min))
-min=j;
-count++;
-}
-if(min!=i)
-{
-t=*(a+min);
-*(a+min)=*(a+i);
-*(a+i)=t;
-}
-}
+void selectionsort(int *a,int n){
+	int i,j,min,t;
+	for(i=0;i<n-1;i++){
+		min=i;
+		for(j=i+1;j<n;j++){
+			if(*(a+j)<*(a+min))
+			min=j;
+			count++;
+		}
+		if(min!=i){
+			t=*(a+min);
+			*(a+min)=*(a+i);
+			*(a+i)=t;
+		}	
+	}	
 }
 void tester()
 {
